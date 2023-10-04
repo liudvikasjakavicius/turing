8731199ac9c315ea23288f0d3068d07a188eb3a0
liudvikasjakavicius <144539295+liudvikasjakavicius@users.noreply.github.com>
2023-10-04 20:57:54 +0300

    Add files via upload
    CS50 - 0 - Playback

---
+++ playback.py
@@ -0,0 +1,6 @@
+# Instructions for the user.
+message = input ("Please type a sentence (minimum of three words):")
+# Change the input white spaces for 3 dots
+new_message = message.replace (" ", "...")
+#Print the output
+print(new_message)
